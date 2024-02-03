bd7db17f226084cba876410e1a5f7fab1efec846
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-03 18:04:58 -0500

    added problem statement

--- prog2.py
+++ prog2.py
@@ -1,2 +1,10 @@
 #this comment is a test
 #yay emacs
+"""
+Problem Statement
+A specialty donation allows you to give more of what patients need in just one appointment. During your donation, one part of your blood is collected, while the rest is returned to your body. This process is called apheresis (AY-fur-EE-sis). (reference: www.mbc.org/donate-blood/about-blood/right-type-your-type/)
+In this decision problem, you are to determine the best types of donation for a particular blood type based on the following table. You must also check if any invalid blood types are entered at the user prompt.
+
+
+link to table: https://ibb.co/NxQmnCC
+"""
